c3f3d458739f8edce695a9ca15c4a173144df2db
grzegorzpnk <grzegorz.panek@orange.com>
2022-12-10 04:14:48 +0100

    new API POST

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package mongoGolang
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
@@ -16,11 +17,18 @@ func main() {
 	client, _ = mongo.Connect(ctx, "mongodb://localhost:27017")
 
 	router := mux.NewRouter()
+	router.HandleFunc("/cluster", CreateClusterHandler).Methods("POST")
 	http.ListenAndServe(":80", router)
 }
 
 func CreateClusterHandler(response http.ResponseWriter, request *http.Request) {
-
+	response.Header().Add("content-type", "application/json")
+	var cluster Cluster
+	json.NewDecoder(request.Body).Decode(&cluster)
+	collection := client.Database("topology").Collection("clusters")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, cluster)
+	json.NewEncoder(response).Encode(result)
 }
 
 type Cluster struct {
