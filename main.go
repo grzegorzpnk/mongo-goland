7e885276f81007310579dc42d159dd060394c5ce
grzegorzpnk <grzegorz.panek@orange.com>
2022-12-10 03:52:18 +0100

    initial status

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package mongoGolang
 
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+	"time"
+)
+
+var client *mongo.Client
+
 func main() {
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, "mongodb://localhost:27017")
+
+	router := mux.NewRouter()
+	http.ListenAndServe(":80", router)
+}
+
+func CreateClusterHandler(response http.ResponseWriter, request *http.Request) {
+
+}
+
+type Cluster struct {
+	name      string
+	resources Resources
+}
+
+type Resources struct {
+	cpu    int
+	memory int
 }
