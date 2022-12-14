0647fb1b896b35c3740ebd51fbfd2aaf9de5bc81
grzegorzpnk <grzegorz.panek@orange.com>
2022-12-14 13:54:31 +0100

    fix warnings

--- src/api/handler.go
+++ src/api/handler.go
@@ -24,44 +24,60 @@ func (h *apiHandler) CreateClusterHandler(response http.ResponseWriter, request 
 	fmt.Println("New request")
 	response.Header().Add("content-type", "application/json")
 	var cluster structure.Cluster
-	json.NewDecoder(request.Body).Decode(&cluster)
+	err := json.NewDecoder(request.Body).Decode(&cluster)
+	if err != nil {
+		fmt.Println(fmt.Errorf("error: " + err.Error()))
+	}
 	fmt.Println(cluster)
 	collection := h.clientMongo.Database("topology").Collection("clusters")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	result, err := collection.InsertOne(ctx, cluster)
 	if err != nil {
 		log.Fatal("BLAD", err)
 	}
-	json.NewEncoder(response).Encode(result)
+	err = json.NewEncoder(response).Encode(result)
+	if err != nil {
+		fmt.Println(fmt.Errorf("error: " + err.Error()))
+	}
 
 }
 
-func (h *apiHandler) GetClustersHandler(response http.ResponseWriter, request *http.Request) {
+func (h *apiHandler) GetClustersHandler(response http.ResponseWriter, _ *http.Request) {
 	fmt.Println("GET Clusters method")
 	response.Header().Add("content-type", "application/json")
 
 	var cluster []structure.Cluster
 
 	collection := h.clientMongo.Database("topology").Collection("clusters")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	cursor, err := collection.Find(ctx, bson.M{})
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{ "message 1": "` + err.Error() + `" }`))
+		_, err2 := response.Write([]byte(`{ "message 1": "` + err.Error() + `" }`))
+		fmt.Println(err2.Error())
 		return
 	}
 
 	for cursor.Next(ctx) {
-		var cluster_tmp structure.Cluster
-		cursor.Decode(&cluster_tmp)
-		cluster = append(cluster, cluster_tmp)
+		var clusterTmp structure.Cluster
+		err := cursor.Decode(&clusterTmp)
+		if err != nil {
+			fmt.Printf("error: " + err.Error())
+		}
+		cluster = append(cluster, clusterTmp)
 	}
 	if err := cursor.Err(); err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{ "message 2": "` + err.Error() + `" }`))
+		_, err2 := response.Write([]byte(`{ "message 2": "` + err.Error() + `" }`))
+		fmt.Println(err2.Error())
 		return
 	}
 
-	json.NewEncoder(response).Encode(cluster)
+	err2 := json.NewEncoder(response).Encode(cluster)
+	if err2 != nil {
+		fmt.Printf("error: " + err2.Error())
+	}
 
 }
